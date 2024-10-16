8c8843698d678449b406b0d169085d77a8312374
Chetan1780 <muljanichetan100@gmail.com>
2024-10-16 16:58:47 +0530

    Home Page bug fix

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Navbar from '@/components/Navbar'
-import Hero from '../components/Hero'
-import Category from '../components/Category'
-import MostSearchedCar from '../components/MostSearchedCar'
-import Section from '../components/Section'
-import Footer from '../components/Footer'
+import Hero from '@/components/Hero'
+import Category from '@/components/Category'
+import MostSearchedCar from '@/components/MostSearchedCar'
+import Section from '@/components/Section'
+import Footer from '@/components/Footer'
 const Home = () => {
   return (
     <div>
