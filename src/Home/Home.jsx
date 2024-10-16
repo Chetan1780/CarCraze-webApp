ab2fe7f4b9b9cbc461dbe6fb0661d0d573d0b119
Chetan1780 <muljanichetan100@gmail.com>
2024-10-16 16:56:50 +0530

    update

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import Navbar from '@/components/Navbar'
 import Hero from '../components/Hero'
 import Category from '../components/Category'
 import MostSearchedCar from '../components/MostSearchedCar'
